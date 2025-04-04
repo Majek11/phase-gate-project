3b9283558d3754f01405a5a24e58262ee2d4e19c
Majek11 <majeklekan9@gmail.com>
2025-04-04 15:16:58 +0100

    test-modified with 28 days and 14 days cycle test cases

--- src/app/MenstrualApp.java
+++ src/app/MenstrualApp.java
@@ -3,25 +3,19 @@ import java.time.LocalDate;
 
 public class MenstrualApp {
     private LocalDate lastMenstrualPeriodDate;
-    private int lengthOfOvulationCycle;
     private int lengthOfMenstruationCycle;
     private int menstruationDuration;
     private int daysBetweenOvulationAndMenstruation;
 
 
-    public MenstrualApp(String ovulationDate, int lengthOfOvulationCycle) {
+    public MenstrualApp(String ovulationDate, int lengthOfMenstruationCycle) {
         this.lastMenstrualPeriodDate = LocalDate.parse(ovulationDate);
-        this.lengthOfOvulationCycle = lengthOfOvulationCycle;
+        this.lengthOfMenstruationCycle = lengthOfMenstruationCycle;
         this.menstruationDuration = 5;
-        this.lengthOfMenstruationCycle = 28;
         this.daysBetweenOvulationAndMenstruation = 0;
     }
 
-    public LocalDate getLastMenstrualPeriodDate() {
-        return lastMenstrualPeriodDate;
-    }
-
     public LocalDate getOvulationDate() {
-        return lastMenstrualPeriodDate.plusDays(lengthOfOvulationCycle);
+        return lastMenstrualPeriodDate.plusDays(lengthOfMenstruationCycle / 2);
     }
 }
--- src/test/TestMenstrualApp.java
+++ src/test/TestMenstrualApp.java
@@ -11,11 +11,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestMenstrualApp {
 
     @Test
-    @DisplayName("Test that ovulation date is valid")
-    public void testValidOvulationDate() {
+    @DisplayName("Test that ovulation date is valid for a 28-day cycle")
+    public void testValidOvulationDateFor28Days() {
         MenstrualApp app = new MenstrualApp("2025-10-06", 28);
-        LocalDate ovulationDate = LocalDate.parse("2025-11-03");
+        LocalDate ovulationDate = LocalDate.parse("2025-10-20");
         assertEquals(ovulationDate, app.getOvulationDate() );
     }
+
+    @Test
+    @DisplayName("Test that ovulation date is valid for a 14-day cycle")
+    public void testValidOvulationDateFor14Days() {
+        MenstrualApp app = new MenstrualApp("2025-10-06", 14);
+        LocalDate ovulationDate = LocalDate.parse("2025-10-13");
+        assertEquals(ovulationDate, app.getOvulationDate() );
+    }
+
+    @Test
+    @DisplayName("Test to calculate safe period")
+    public void testToCalculateSafePeriod() {
+        MenstrualApp app = new MenstrualApp("2025-10-06",  28);
+        LocalDate safePeriodStart = LocalDate.parse("2025-10-06");
+        LocalDate safePeriodEnd = LocalDate.parse("2025-10-13");
+
+    }
+
 
 }
